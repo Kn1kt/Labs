3fba640f515ddf259531c82611d539d1730fd44c
Nikita <47992492+Kn1kt@users.noreply.github.com>
2020-02-27 00:15:02 +0300

    lab3

---
+++ Queue.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+public protocol Queue {
+  associatedtype Element
+  mutating func enqueue(_ element: Element) -> Bool
+  mutating func dequeue() -> Element?
+  var isEmpty: Bool { get }
+  var peek: Element? { get }
+}
+
+public struct QueueStack<T>: Queue {
+  
+  private var leftStack: [T] = []
+  private var rightStack: [T] = []
+  
+  public init() {}
+  
+  public var isEmpty: Bool {
+    return leftStack.isEmpty && rightStack.isEmpty
+  }
+  
+  public var peek: T? {
+    return !leftStack.isEmpty ? leftStack.last : rightStack.first
+  }
+  
+  @discardableResult
+  public mutating func enqueue(_ element: T) -> Bool {
+    rightStack.append(element)
+    return true
+  }
+  
+  public mutating func dequeue() -> T? {
+    if leftStack.isEmpty {
+      leftStack = rightStack.reversed()
+      rightStack.removeAll()
+    }
+    return leftStack.popLast()
+  }
+}
+

e0d37a5a1d86f294e36e8552aedcd7d2121c16a0
Kn1kt <47992492+Kn1kt@users.noreply.github.com>
2020-03-25 15:50:12 +0300

    add light weight iterators

--- lab2/AVLTreeIterator.swift
+++ lab2/AVLTreeIterator.swift
@@ -1,31 +1,120 @@
 import Foundation
 
 public class AVLTreeIterator<Element: Comparable>: IteratorProtocol {
- 
+  
   public enum Traverse {
     case inOrder
     case preOrder
     case postOrder
   }
   
-  private var elements = [Element]()
-  private var index = 0
+  private let type: Traverse!
+  private var currentNode: AVLNode<Element>?
   
   public init(tree: AVLTree<Element>, type: Traverse) {
+    self.type = type
     guard let root = tree.root else { return }
+    
     switch type {
     case .preOrder:
-      root.traversePreOrder { elements.append($0) }
-    case .inOrder:
-      root.traverseInOrder { elements.append($0) }
-    case .postOrder:
-      root.traversePostOrder { elements.append($0) }
+       currentNode = root
+    default:
+      currentNode = root
+      while let left = currentNode?.leftChild {
+        currentNode = left
+      }
     }
   }
   
   public func next() -> Element? {
-    defer { index += 1 }
-    return index < elements.count ? elements[index] : nil
+    guard let _ = currentNode else { return nil }
+    
+    switch type {
+    case .inOrder:
+      return traverseInOrder()
+    case .preOrder:
+      return traversePreOrder()
+    case .postOrder:
+      return traversePostOrder()
+    case .none:
+      fatalError()
+    }
   }
   
+  private func traverseInOrder() -> Element {
+    let result = currentNode!.value
+    
+    if var pivot = currentNode?.rightChild {
+      
+      while let left = pivot.leftChild {
+        pivot = left
+      }
+      currentNode = pivot
+      
+    } else {
+    
+      while true {
+        guard let parent = currentNode?.parent else {
+          currentNode = nil
+          break
+        }
+        
+        if currentNode === parent.leftChild {
+          currentNode = parent
+          break
+        }
+        
+        currentNode = parent
+      }
+    }
+    
+    return result
+  }
+  
+  private func traversePreOrder() -> Element {
+    let result = currentNode!.value
+    
+    if let left = currentNode?.leftChild {
+      currentNode = left
+    } else {
+      
+      while true {
+        guard let parent = currentNode?.parent else {
+          currentNode = nil
+          break
+        }
+        
+        if currentNode === parent.leftChild,
+          let right = parent.rightChild {
+          currentNode = right
+          break
+        }
+        
+        currentNode = parent
+      }
+    }
+    
+    return result
+  }
+  
+  private func traversePostOrder() -> Element {
+    let result = currentNode!.value
+    
+    if let parent = currentNode?.parent {
+      if parent.leftChild === currentNode {
+        var pivot = parent.rightChild
+        while let left = pivot?.leftChild {
+          pivot = left
+        }
+        
+        currentNode = pivot
+      } else {
+        currentNode = parent
+      }
+    } else {
+      currentNode = nil
+    }
+    
+    return result
+  }
 }
